f4810bfe74134b927043efd8293c9ac3b8c0467c
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-02-24 17:53:28 +0100

    Final cleanup for today.. I'm sorry but I gotta get going.

--- engine-core-impl/src/main/java/de/hpi/oryxengine/routingBehaviour/AbstractRoutingBehaviour.java
+++ engine-core-impl/src/main/java/de/hpi/oryxengine/routingBehaviour/AbstractRoutingBehaviour.java
@@ -6,11 +6,26 @@ import de.hpi.oryxengine.joinBehaviour.JoinBehaviour;
 import de.hpi.oryxengine.processInstance.ProcessInstance;
 import de.hpi.oryxengine.splitBehaviour.SplitBehaviour;
 
+/**
+ * The Class AbstractRoutingBehaviour.
+ */
 public abstract class AbstractRoutingBehaviour implements RoutingBehaviour {
 
+    /** The join behaviour. */
     protected JoinBehaviour joinBehaviour;
+
+    /** The split behaviour. */
     protected SplitBehaviour splitBehaviour;
 
+    /**
+     * Behaves like the routing behaviour should behave. That is joining and splitting as defined.
+     * 
+     * @param instance
+     *            the instance
+     * @return the list
+     * @see 
+     * de.hpi.oryxengine.routingBehaviour.RoutingBehaviour#execute(de.hpi.oryxengine.processInstance.ProcessInstance)
+     */
     public List<ProcessInstance> execute(ProcessInstance instance) {
 
         List<ProcessInstance> joinedInstances = joinBehaviour.join(instance);
