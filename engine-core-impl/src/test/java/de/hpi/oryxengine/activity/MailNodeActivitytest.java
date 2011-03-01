d40862b36dd02701d259fe7bc0c6d40e7ccc9892
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-03-01 09:37:17 +0100

    Errm. I kind of forgot to check in my test. Sorry. It's the mailnode test.

---
+++ engine-core-impl/src/test/java/de/hpi/oryxengine/activity/MailNodeActivitytest.java
@@ -0,0 +1,62 @@
+package de.hpi.oryxengine.activity;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.Iterator;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.dumbster.smtp.SimpleSmtpServer;
+import com.dumbster.smtp.SmtpMessage;
+
+import de.hpi.oryxengine.factory.MailNodeFactory;
+import de.hpi.oryxengine.factory.SimpleProcessInstanceFactory;
+import de.hpi.oryxengine.process.instance.ProcessInstance;
+import de.hpi.oryxengine.process.structure.Node;
+
+/**
+ * The Class MailNodeActivitytest.
+ * Which simply uses a fake mail server to send a mail and tests if the content is right
+ */
+public class MailNodeActivitytest {
+  
+  /** The mailernode. */
+  private Node mailernode;
+  
+  /** The p. */
+  private ProcessInstance p;
+  
+  /** The maily. */
+  private SimpleSmtpServer maily;
+  
+  /** The SMTP port. */
+  private final static int SMTP_PORT = 2525;
+  
+  /**
+   * Sets the up.
+   */
+  @BeforeTest
+  public void setUp() {
+      MailNodeFactory factory = new MailNodeFactory();
+      mailernode = factory.create();
+      SimpleProcessInstanceFactory processfactory = new SimpleProcessInstanceFactory(); 
+      p = processfactory.create(mailernode);
+      p.setVariable("result", "Roflcopter123!");
+      maily = SimpleSmtpServer.start(SMTP_PORT);
+  }
+  
+  /**
+   * Test the sending of a mail.
+   */
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testMailsend() {
+      p.executeStep();
+      maily.stop();
+      assertEquals(maily.getReceivedEmailSize(), 1, "Upps we didn't receive an email.. too bad");
+      Iterator<SmtpMessage> emailIter = maily.getReceivedEmail();
+      SmtpMessage email = (SmtpMessage) emailIter.next();
+      assertEquals(email.getBody().contains("Roflcopter123!"), true, "No Roflcopter on the fly...");
+  }
+}
