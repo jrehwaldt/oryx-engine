a8562f8afb9b774375349de5e1ad6ebb055da391
thorben.lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2011-03-03 14:50:37 +0100

    Added a lot of Javadoc, removed unused imports and had other warning fun.

--- engine-core-impl/src/main/java/de/hpi/oryxengine/monitor/BarPaintSurface.java
+++ engine-core-impl/src/main/java/de/hpi/oryxengine/monitor/BarPaintSurface.java
@@ -2,12 +2,6 @@ package de.hpi.oryxengine.monitor;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.JPanel;
-
-import de.hpi.oryxengine.process.instance.ProcessInstance;
 
 /**
  * The Class PaintSurface.
@@ -16,17 +10,22 @@ public class BarPaintSurface extends PaintSurface {
 
     private int runningInstances;
     private int maximumInstances;
-    
-    //The delta of process instances being removed/added to the scheduler in order to trigger a GUI update.
-    private int REPAINT_TOLERANCE = 200;
-    private int NUMBER_OF_BARS = 15;
 
+    // The delta of process instances being removed/added to the scheduler in order to trigger a GUI update.
+    private final static int REPAINT_TOLERANCE = 200;
+    private final static int NUMBER_OF_BARS = 15;
+
+    /**
+     * Instantiates a new bar paint surface.
+     *
+     * @param maximumInstances the maximum instances
+     */
     public BarPaintSurface(int maximumInstances) {
 
         super();
         this.runningInstances = 0;
         this.maximumInstances = maximumInstances;
-        
+
     }
 
     /**
@@ -34,6 +33,7 @@ public class BarPaintSurface extends PaintSurface {
      */
     @Override
     protected void paintComponent(Graphics g) {
+
         super.paintComponent(g);
 
         double overallBarWidth = ((double) width) / NUMBER_OF_BARS;
@@ -45,9 +45,20 @@ public class BarPaintSurface extends PaintSurface {
 
         g.setColor(Color.BLACK);
 
-        g.drawString("ProcessInstances in Queue: " + new Integer(runningInstances).toString() + "/" + new Integer(maximumInstances).toString(), 10, 10);
+        g.drawString("ProcessInstances in Queue: " + new Integer(runningInstances).toString() + "/"
+            + new Integer(maximumInstances).toString(), 10, 10);
     }
 
+    /**
+     * Paint a single bar.
+     * 
+     * @param g
+     *            the Graphics object that paints everything.
+     * @param x
+     *            the x
+     * @param y
+     *            the y
+     */
     private void paintSingleBar(Graphics g, int x, int y) {
 
         int barHeight = (int) (((double) height) * 0.9);
@@ -57,6 +68,11 @@ public class BarPaintSurface extends PaintSurface {
         g.fillRect(x, y, barWidth, barHeight);
     }
 
+    /**
+     * The number of bars that have to be drawn in this particular drawing step.
+     * 
+     * @return the int
+     */
     private int numberOfBars() {
 
         // we are calculating with int, so order of operations is important
@@ -66,32 +82,55 @@ public class BarPaintSurface extends PaintSurface {
         return (runningInstances * NUMBER_OF_BARS) / maximumInstances;
     }
 
-    private void paintBackground(Graphics g) {
-
-        g.setColor(Color.GRAY);
-        g.fillRect(0, 0, width, height);
-    }
-
+    /**
+     * Gets the running instances.
+     * 
+     * @return the running instances
+     */
     public int getRunningInstances() {
 
         return runningInstances;
     }
 
+    /**
+     * Sets the running instances.
+     * 
+     * @param runningInstances
+     *            the new running instances
+     */
     public void setRunningInstances(int runningInstances) {
 
         this.runningInstances = runningInstances;
     }
 
+    /**
+     * Gets the maximum instances.
+     * 
+     * @return the maximum instances
+     */
     public int getMaximumInstances() {
 
         return maximumInstances;
     }
 
+    /**
+     * Sets the maximum instances.
+     * 
+     * @param maximumInstances
+     *            the new maximum instances
+     */
     public void setMaximumInstances(int maximumInstances) {
 
         this.maximumInstances = maximumInstances;
     }
 
+    /**
+     * Updates instances. Only repaints the Panel, if the delta of old instances and new instances is greater than the
+     * defined one.
+     * 
+     * @param runningInstances
+     *            the running instances
+     */
     public void updateInstances(int runningInstances) {
 
         // only update and repaint, if the difference is at least beyond a given repaint-tolerance.
--- engine-core-impl/src/main/java/de/hpi/oryxengine/monitor/MonitorGUI.java
+++ engine-core-impl/src/main/java/de/hpi/oryxengine/monitor/MonitorGUI.java
@@ -2,12 +2,9 @@ package de.hpi.oryxengine.monitor;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
 
-import de.hpi.oryxengine.navigator.Navigator;
-import de.hpi.oryxengine.navigator.NavigatorImpl;
 import de.hpi.oryxengine.navigator.schedule.SchedulerAction;
 import de.hpi.oryxengine.navigator.schedule.SchedulerEvent;
 import de.hpi.oryxengine.process.instance.ProcessInstance;
@@ -20,11 +17,21 @@ public class MonitorGUI extends JFrame {
     private static BarPaintSurface barSurface;
     private static InstanceNotifyPanel notifySurface;
 
+    /**
+     * Instantiates a new monitor gui.
+     */
     public MonitorGUI() {
 
         super("Monitor");
     }
 
+    /**
+     * Start the monitor GUI. Renders a new JFrame.
+     * 
+     * @param maximumInstances
+     *            the highest number of instances that can occur in the scheduler.
+     * @return the monitor gui
+     */
     public static MonitorGUI start(int maximumInstances) {
 
         MonitorGUI monitor = new MonitorGUI();
@@ -45,22 +52,45 @@ public class MonitorGUI extends JFrame {
         return monitor;
     }
 
+    /**
+     * Gets the paint surface.
+     * 
+     * @return the paint surface
+     */
     public BarPaintSurface getPaintSurface() {
 
         return barSurface;
     }
 
+    /**
+     * Tell the PaintSurface that draws the Scheduler-Bars that the number of instances in the scheduler has changed.
+     * 
+     * @param numberOfInstances
+     *            the number of instances
+     */
     public void updateNumberOfInstances(int numberOfInstances) {
 
         barSurface.updateInstances(numberOfInstances);
     }
 
+    /**
+     * An instance has been retrieved from the scheduler. Show it in the GUI.
+     * 
+     * @param instance
+     *            the instance
+     */
     public void showInstanceRetrieved(ProcessInstance instance) {
 
         SchedulerEvent event = new SchedulerEvent(SchedulerAction.RETRIEVE, instance, -1);
         notifySurface.addNotification(event);
     }
 
+    /**
+     * An instance has been submitted to the scheduler. Show it in the GUI.
+     * 
+     * @param instance
+     *            the instance
+     */
     public void showInstanceSubmitted(ProcessInstance instance) {
 
         SchedulerEvent event = new SchedulerEvent(SchedulerAction.SUBMIT, instance, -1);
