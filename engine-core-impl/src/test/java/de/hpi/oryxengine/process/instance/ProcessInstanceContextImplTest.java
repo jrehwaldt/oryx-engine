1bd2f5c6f7da8854169186ba59b2c3b1acd7061f
JannikStreek <theshowcanbegin@gmail.com>
2011-03-08 15:55:50 +0100

    added some tests for context object

--- engine-core-impl/src/test/java/de/hpi/oryxengine/process/instance/ProcessInstanceContextImplTest.java
+++ engine-core-impl/src/test/java/de/hpi/oryxengine/process/instance/ProcessInstanceContextImplTest.java
@@ -1,33 +1,135 @@
 package de.hpi.oryxengine.process.instance;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
+import java.util.ArrayList;
+
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import de.hpi.oryxengine.process.structure.Node;
+import de.hpi.oryxengine.process.structure.Transition;
 
 /**
  * The Class ProcessInstanceContextImplTest.
  */
 public class ProcessInstanceContextImplTest {
 
-    /** The list. */
-    ArrayList<String> list;
-
-    /** The list2. */
-    ArrayList<String> list2;
+    /** The context. */
+    private ProcessInstanceContext context;
 
     /**
-     * F.
+     * Sets the waiting execution with empty waiting transitions. This is the second part of the method which is tested.
+     * For a given transition, the destination node is used as key. A List with the Transition is expected as return
+     * value.
      */
     @Test
-    public void f() {
+    public void setWaitingExecutionWithEmptyWaitingTransitions() {
 
-        list.removeAll(list2);
-        assertEquals(list.size(), 1);
+        Transition t = mock(Transition.class);
+
+        Node n = mock(Node.class);
+        when(t.getDestination()).thenReturn(n);
+
+        context.setWaitingExecution(t);
+
+        ArrayList<Transition> expected = new ArrayList<Transition>();
+        expected.add(t);
+        assertEquals(context.getWaitingExecutions(n), expected);
+
+    }
+
+    /**
+     * Sets the waiting execution. This is the first part of the method which is tested. See explanation above.
+     */
+    @Test
+    public void setWaitingExecution() {
+
+        Transition t = mock(Transition.class);
+        Transition t2 = mock(Transition.class);
+
+        Node n = mock(Node.class);
+        when(t.getDestination()).thenReturn(n);
+        when(t2.getDestination()).thenReturn(n);
+
+        context.setWaitingExecution(t);
+        context.setWaitingExecution(t2);
+
+        ArrayList<Transition> expected = new ArrayList<Transition>();
+        expected.add(t);
+        expected.add(t2);
+        assertEquals(context.getWaitingExecutions(n), expected);
+    }
+
+    /**
+     * Removes the incoming transitions signaled.
+     */
+    @Test
+    public void removeIncomingTransitionsSignaled() {
+
+        // Create a transition mock with a Destination Node as Mock
+        Transition t = mock(Transition.class);
+        Node n = mock(Node.class);
+        when(t.getDestination()).thenReturn(n);
+
+        // Save the transition in the context Map
+        context.setWaitingExecution(t);
+
+        // Build a list out of it
+        ArrayList<Transition> transitionList = new ArrayList<Transition>();
+        transitionList.add(t);
+
+        // Return the list, if for incoming Transitions is asked
+        when(n.getIncomingTransitions()).thenReturn(transitionList);
+
+        // The method to test, finally...
+        context.removeIncomingTransitions(n);
+
+        // Now the list should be empty, because the remove private method was called in the context
+        assertEquals(context.getWaitingExecutions(n).size(), 0);
+    }
+
+    /**
+     * All incoming transitions signaled - but without signaled transitions.
+     */
+    @Test
+    public void allIncomingTransitionsSignaledWithoutSignaledTransitions() {
+
+        Node n = mock(Node.class);
+        assertFalse(context.allIncomingTransitionsSignaled(n), "should not contain all incoming transitions");
+    }
+
+    /**
+     * All incoming transitions are signaled and therefore true should be returned .
+     */
+    @Test
+    public void allIncomingTransitionsSignaled() {
+
+        // Create a transition mock with a Destination Node as Mock
+        Transition t = mock(Transition.class);
+        Transition t2 = mock(Transition.class);
+        
+        Node n = mock(Node.class);
+        
+        when(t.getDestination()).thenReturn(n);
+        when(t2.getDestination()).thenReturn(n);
+        
+        context.setWaitingExecution(t);
+        context.setWaitingExecution(t2);
+
+        // Build a list out of it
+        ArrayList<Transition> transitionList = new ArrayList<Transition>();
+        transitionList.add(t);
+
+        // Return the list, if for incoming Transitions is asked
+        when(n.getIncomingTransitions()).thenReturn(transitionList);
+
+        assertTrue(context.allIncomingTransitionsSignaled(n), "should contain all incoming transitions");
     }
 
     /**
@@ -36,17 +138,7 @@ public class ProcessInstanceContextImplTest {
     @BeforeClass
     public void beforeClass() {
 
-        String a = "a";
-        String b = "b";
-
-        list = new ArrayList<String>();
-        list2 = new ArrayList<String>();
-
-        list.add(a);
-        list.add(a);
-        list.add(b);
-        list2.add(a);
-        list2.add(b);
+        context = new ProcessInstanceContextImpl();
     }
 
     /**
@@ -54,6 +146,8 @@ public class ProcessInstanceContextImplTest {
      */
     @AfterClass
     public void afterClass() {
+        
+        context = null;
 
     }
 
