6c5fc537bfec705af9fb3102f5b6dac10c7faa7b
Tobias Metzke <tobias.metzke@googlemail.com>
2011-03-10 14:10:25 +0100

    moved load generator;

--- engine-core-impl/src/test/java/de/hpi/oryxengine/loadgenerator/LoadGeneratorWindow.java
+++ engine-core-impl/src/test/java/de/hpi/oryxengine/loadgenerator/LoadGeneratorWindow.java
@@ -1,4 +1,4 @@
-package de.hpi.oryxengine.loadGenerator;
+package de.hpi.oryxengine.loadgenerator;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.io.FileNotFoundException;
 import java.text.NumberFormat;
 
 import javax.swing.BorderFactory;
@@ -17,35 +16,28 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-
-import de.hpi.oryxengine.loadgenerator.LoadGenerator;
 
 /**
  * The Class LoadGeneratorWindow.
  */
-public class LoadGeneratorWindow 
-extends JPanel
-implements ActionListener, PropertyChangeListener {
-    
+public class LoadGeneratorWindow extends JPanel implements ActionListener, PropertyChangeListener {
+
     private static final long serialVersionUID = 1L;
 
     // window components
     private JComboBox processModelBox;
-    private JTextField processModelText;
     private JFormattedTextField numberOfThreads;
     private JFormattedTextField numberOfInstances;
     private final JButton startButton;
-    
+
     // format values for formatted text fields
     private NumberFormat amountFormat;
-    
+
     // default values to be shown in window
     private static final int DEFAULT_THREAD_AMOUNT = 10;
     private static final int DEFAULT_INSTANCE_AMOUNT = 10000;
-	//private static final String PATH_TO_PROCESSES = "undefined";
     private String[] processes;
-    
+
     // variables to be passed on to Load Generator
     private int threads;
     private int instances;
@@ -55,7 +47,7 @@ implements ActionListener, PropertyChangeListener {
      * Instantiates a new load generator window.
      */
     public LoadGeneratorWindow() {
-       
+
         super(new BorderLayout());
         threads = DEFAULT_THREAD_AMOUNT;
         instances = DEFAULT_INSTANCE_AMOUNT;
@@ -74,17 +66,17 @@ implements ActionListener, PropertyChangeListener {
         numberOfThreads.addPropertyChangeListener("value", this);
         JLabel threadsLabel = new JLabel("number of navigator threads: ");
         threadsLabel.setLabelFor(numberOfThreads);
-        
+
         // Create field for number of process instances
         numberOfInstances = new JFormattedTextField(amountFormat);
         numberOfInstances.setValue(instances);
         numberOfInstances.addPropertyChangeListener("value", this);
         JLabel instancesLabel = new JLabel("number of process instances: ");
         instancesLabel.setLabelFor(numberOfInstances);
-        
+
         // Create a check box for showing monitor afterwards
         JCheckBox showMonitor = new JCheckBox("show monitor afterwards", false);
-        
+
         // Create a start button for starting the load generator
         startButton = new JButton("Start");
         startButton.addActionListener(this);
@@ -92,13 +84,13 @@ implements ActionListener, PropertyChangeListener {
         // -------------------- Area for creating panels like text field areas or button areas --------------------
         // Create a text field area
         JPanel textFieldArea = new JPanel(new GridLayout(0, 2));
-        textFieldArea.setBorder(
-            BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Navigator configuration"));
-        
+        textFieldArea.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
+            "Navigator configuration"));
+
         // Create an area for the check box and a start button
         JPanel startingArea = new JPanel(new GridLayout(0, 1));
-        startingArea.setBorder(BorderFactory.createEtchedBorder());     
-        
+        startingArea.setBorder(BorderFactory.createEtchedBorder());
+
         // -------------------- Area for putting the things together --------------------
         textFieldArea.add(processesLabel);
         textFieldArea.add(processModelBox);
@@ -112,53 +104,46 @@ implements ActionListener, PropertyChangeListener {
         this.add(startingArea, BorderLayout.SOUTH);
     }
 
+    /**
+     * Find the example processes available for generating load.
+     * 
+     * @return the array of process names
+     */
     private String[] findProcesses() {
-    	/*File dir = new File(PATH_TO_PROCESSES);
-    	File[] fileList = dir.listFiles();
-    	System.out.println(fileList);
-    	for(File f : fileList) {
-    	    System.out.println(f.getName());
-    	}*/
-    	String[] processModels = {"ExampleProcessTokenFactory", "HeavyComputationProcessTokenFactory"};
-    	return processModels;
-	}
-    
-    private void saveProperty(String key, String value) {
-		// TODO save to properties file
-		
-	}
 
-	@Override
-    public void actionPerformed(ActionEvent e) {
-
-      //Handle start button action.
-        if (e.getSource() == startButton) {
-        	saveProperty("processMoped",processModel);
-        	saveProperty("numberOfThreads", String.valueOf(threads));
-        	saveProperty("numerOfInstances", String.valueOf(instances));
-        	LoadGenerator gene;
-			try {
-				gene = new LoadGenerator();
-				gene.execute();
-			} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-        // Handle the combo box	
-        }else if (e.getSource() == processModelBox) {
-        	JComboBox box = (JComboBox)e.getSource();
-            processModel = (String)box.getSelectedItem();
-		}
+        /*
+         * File dir = new File(PATH_TO_PROCESSES); File[] fileList = dir.listFiles(); System.out.println(fileList);
+         * for(File f : fileList) { System.out.println(f.getName()); }
+         */
+        String[] processModels = {"ExampleProcessTokenFactory", "HeavyComputationProcessTokenFactory"};
+        return processModels;
     }
 
-	@Override
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        // Handle start button action.
+        if (e.getSource() == startButton) {
+            System.out.println(instances);
+            LoadGenerator gene = new LoadGenerator(processModel, instances, threads);
+            gene.execute();
+        // Handle the combo box
+        } else if (e.getSource() == processModelBox) {
+            JComboBox box = (JComboBox) e.getSource();
+            processModel = (String) box.getSelectedItem();
+        }
+    }
+
+    @Override
     public void propertyChange(PropertyChangeEvent evt) {
 
         Object source = evt.getSource();
         if (source == numberOfInstances) {
             instances = ((Long) numberOfInstances.getValue()).intValue();
+            System.out.println(instances);
         } else if (source == numberOfThreads) {
             threads = ((Long) numberOfThreads.getValue()).intValue();
+            System.out.println(threads);
         }
     }
 
@@ -166,10 +151,11 @@ implements ActionListener, PropertyChangeListener {
      * Create the GUI and show it.
      */
     private static void createAndShowGUI() {
+
         // Create and set up the window.
         JFrame frame = new JFrame("Load Generator GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         // add the panel that is defined in the constructor
         frame.setContentPane(new LoadGeneratorWindow());
 
@@ -177,17 +163,20 @@ implements ActionListener, PropertyChangeListener {
         frame.pack();
         frame.setVisible(true);
     }
-    
+
     /**
      * The main method. Creates and starts the window.
-     *
-     * @param args the arguments
+     * 
+     * @param args
+     *            the arguments
      */
     public static void main(String[] args) {
+
         // Schedule a job for the event-dispatching thread:
         // creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+
                 createAndShowGUI();
             }
         });
