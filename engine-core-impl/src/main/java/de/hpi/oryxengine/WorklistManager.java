e9f6e443a87bad55ab1cfd6c929d930d6484b2eb
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2011-03-15 17:49:28 +0100

    Token Interfaced veraendert

--- engine-core-impl/src/main/java/de/hpi/oryxengine/WorklistManager.java
+++ engine-core-impl/src/main/java/de/hpi/oryxengine/WorklistManager.java
@@ -154,7 +154,6 @@ public class WorklistManager implements WorklistService, TaskDistribution, Workl
         for (Resource<?> resource : worklistItemImpl.getAssignedResources()) {
             getWorklistItems(resource).remove(worklistItemImpl);
         }
-        worklistItemImpl.
     }
 
     @Override
