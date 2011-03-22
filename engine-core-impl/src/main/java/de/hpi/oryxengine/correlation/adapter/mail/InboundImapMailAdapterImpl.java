144c63e27ccdf5cb98521f095f3b3d8569036a0f
thorben.lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2011-03-22 16:51:03 +0100

    Ein neuer Prozess wird nur instanziiert solange die entsprechende Mail ungelesen ist.

--- engine-core-impl/src/main/java/de/hpi/oryxengine/correlation/adapter/mail/InboundImapMailAdapterImpl.java
+++ engine-core-impl/src/main/java/de/hpi/oryxengine/correlation/adapter/mail/InboundImapMailAdapterImpl.java
@@ -3,6 +3,8 @@ package de.hpi.oryxengine.correlation.adapter.mail;
 import java.io.IOException;
 
 import javax.annotation.Nonnull;
+import javax.mail.Flags;
+import javax.mail.Flags.Flag;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -16,13 +18,12 @@ import de.hpi.oryxengine.exception.DalmatinaException;
 
 /**
  * This is the default imap mail client implementation and acts as {@link CorrelationAdapter} for the.
- *
+ * 
  * {@link CorrelationManager}.
  */
-public class InboundImapMailAdapterImpl
-extends AbstractCorrelationAdapter<MailAdapterConfiguration>
-implements InboundPullAdapter {
-    
+public class InboundImapMailAdapterImpl extends AbstractCorrelationAdapter<MailAdapterConfiguration> implements
+InboundPullAdapter {
+
     /**
      * Default constructor.
      * 
@@ -34,14 +35,13 @@ implements InboundPullAdapter {
     @SuppressWarnings("restriction")
     public InboundImapMailAdapterImpl(@Nonnull CorrelationManager correlation,
                                       @Nonnull MailAdapterConfiguration configuration) {
+
         super(correlation, configuration);
-        
+
         this.logger.info("MailAdapter initialized with config: {}", this.configuration);
-        
+
         if (this.configuration.isUseSSL()) {
-            java.security.Security.addProvider(
-                new com.sun.net.ssl.internal.ssl.Provider()
-             );
+            java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
         }
     }
 
@@ -53,44 +53,43 @@ implements InboundPullAdapter {
     @Override
     public void pull()
     throws DalmatinaException {
-        
+
         try {
             Session session = Session.getInstance(this.configuration.toMailProperties());
-            
-    //        logger.debug("getting the session for accessing email.");
+
+            // logger.debug("getting the session for accessing email.");
             Store store = session.getStore("imap");
-            
-            store.connect(
-                this.configuration.getAddress(),
-                this.configuration.getUserName(),
-                this.configuration.getPassword()
-            );
-    //        logger.debug("Connection established with IMAP server.");
-            
+
+            store.connect(this.configuration.getAddress(), this.configuration.getUserName(),
+                this.configuration.getPassword());
+            // logger.debug("Connection established with IMAP server.");
+
             // Get a handle on the default folder
             Folder folder = store.getDefaultFolder();
-            
-    //        logger.debug("Getting the Inbox folder.");
-            
+
+            // logger.debug("Getting the Inbox folder.");
+
             // Retrieve the "Inbox"
             folder = folder.getFolder("inbox");
-            
+
             // Reading the Email Index in Read / Write Mode
             folder.open(Folder.READ_WRITE);
-            
+
             // Retrieve the messages
             Message[] messages = folder.getMessages();
-            
+
             logger.debug("Found {} messages", messages.length);
-            
-            // Loop over all of the messages
-            for (Message message: messages) {
-                processMessage(message);
+
+            // Loop over all of the messages. Only process them if they are new.
+            for (Message message : messages) {
+                if (!message.isSet(Flag.SEEN)) {
+                    processMessage(message);
+                }
             }
-            
+
             // Close the folder
             folder.close(true);
-            
+
             // Close the message store
             store.close();
         } catch (MessagingException me) {
@@ -102,13 +101,17 @@ implements InboundPullAdapter {
 
     /**
      * Called for message processing.
-     *
-     * @param message a message to process
-     * @throws IOException thrown if fetching the message content fails
-     * @throws MessagingException thrown if handling the message fails
+     * 
+     * @param message
+     *            a message to process
+     * @throws IOException
+     *             thrown if fetching the message content fails
+     * @throws MessagingException
+     *             thrown if handling the message fails
      */
     private void processMessage(@Nonnull Message message)
     throws IOException, MessagingException {
+
         correlate(new MailAdapterEvent(this.configuration, message));
     }
 }
