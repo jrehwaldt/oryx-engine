b0c5162913def03b8dd245622cd6245cea7b27db
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-04-05 14:22:45 +0200

    Forgot to add our newly created classes

---
+++ engine-core-api/src/main/java/de/hpi/oryxengine/correlation/adapter/TimedConfiguration.java
@@ -0,0 +1,29 @@
+package de.hpi.oryxengine.correlation.adapter;
+
+import javax.annotation.Nonnegative;
+
+import org.quartz.Job;
+
+import de.hpi.oryxengine.correlation.EventConfiguration;
+
+/**
+ * The Interface TimedAdapterConfiguration. This should be used for adapters or events
+ * which have to be called in specific time intervals.
+ * @author Jannik Streek
+ */
+public interface TimedConfiguration extends EventConfiguration {
+        
+        /**
+         * Returns the adapter's time interval in ms.
+         * 
+         * @return the pull interval in ms
+         */
+        @Nonnegative long getTimeInterval();
+        
+        /**
+         * Gets the scheduled class which is used for the QUARTZ Scheduler.
+         *
+         * @return the scheduled class
+         */
+        Class<? extends Job> getScheduledClass();
+}
---
+++ engine-core-impl/src/main/java/de/hpi/oryxengine/correlation/adapter/EventTypes.java
@@ -0,0 +1,37 @@
+package de.hpi.oryxengine.correlation.adapter;
+
+import java.util.UUID;
+
+import javax.annotation.Nonnull;
+
+
+/**
+ * Adapter types.
+ * 
+ * @author Jan Rehwaldt
+ */
+public enum EventTypes
+implements EventType {
+    Mail,
+    Error,
+    Timer;
+    
+    private final @Nonnull UUID id;
+    
+    /**
+     * Default constructor.
+     */
+    private EventTypes() {
+        this.id = UUID.randomUUID();
+    }
+    
+    @Override
+    public @Nonnull UUID getID() {
+        return this.id;
+    }
+    
+    @Override
+    public @Nonnull String getName() {
+        return name();
+    }
+}
