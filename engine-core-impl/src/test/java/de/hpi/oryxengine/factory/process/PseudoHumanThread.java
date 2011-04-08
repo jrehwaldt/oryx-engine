7d7db40966cc63e5aa7304f575c05c6a800c2493
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-04-08 09:09:43 +0200

    Added some javaDoc lazy Jannik wasn't willing to add

--- engine-core-impl/src/test/java/de/hpi/oryxengine/factory/process/PseudoHumanThread.java
+++ engine-core-impl/src/test/java/de/hpi/oryxengine/factory/process/PseudoHumanThread.java
@@ -5,28 +5,33 @@ import java.util.Set;
 import de.hpi.oryxengine.ServiceFactory;
 import de.hpi.oryxengine.resource.Participant;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class PseudoHumanThread.
+ * The Class PseudoHumanThread is used to simulate human behaviour on Worklists. We use it for benchmarking our human
+ * tasks. As is, it is used in processes that have human tasks in the load generator.
  */
-public class PseudoHumanThread extends Thread{
+public class PseudoHumanThread extends Thread {
     private String name;
-    
-    public PseudoHumanThread (String name) {
+
+    /**
+     * Instantiates a new pseudo human thread with the corresponding name.
+     * 
+     * @param name
+     *            the name of the pseudo human thread
+     */
+    public PseudoHumanThread(String name) {
+
         this.name = name;
     }
-    
+
     /**
      * The run method which starts the execution of the thread.
      * 
      * @see java.lang.Thread#run()
      */
     public void run() {
+
         Set<Participant> participants = ServiceFactory.getIdentityService().getParticipants();
-        
 
     }
-
-
 
 }
