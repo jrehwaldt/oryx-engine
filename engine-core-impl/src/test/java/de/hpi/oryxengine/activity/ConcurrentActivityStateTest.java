39c41353aeb26af44e4c9179c4314da159edf2c0
thorben.lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2011-04-12 16:13:55 +0200

    wrote a test for concurrent activty state changes. quite a drawback in design right now.

---
+++ engine-core-impl/src/test/java/de/hpi/oryxengine/activity/ConcurrentActivityStateTest.java
@@ -0,0 +1,75 @@
+package de.hpi.oryxengine.activity;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import de.hpi.oryxengine.activity.impl.NullActivity;
+import de.hpi.oryxengine.exception.DalmatinaException;
+import de.hpi.oryxengine.exception.IllegalStarteventException;
+import de.hpi.oryxengine.navigator.NavigatorImplMock;
+import de.hpi.oryxengine.process.definition.NodeParameter;
+import de.hpi.oryxengine.process.definition.NodeParameterImpl;
+import de.hpi.oryxengine.process.definition.ProcessBuilder;
+import de.hpi.oryxengine.process.definition.ProcessBuilderImpl;
+import de.hpi.oryxengine.process.definition.ProcessDefinition;
+import de.hpi.oryxengine.process.instance.ProcessInstance;
+import de.hpi.oryxengine.process.instance.ProcessInstanceImpl;
+import de.hpi.oryxengine.process.structure.Node;
+import de.hpi.oryxengine.process.token.Token;
+import de.hpi.oryxengine.routing.behaviour.incoming.impl.SimpleJoinBehaviour;
+import de.hpi.oryxengine.routing.behaviour.outgoing.impl.TakeAllSplitBehaviour;
+
+/**
+ * The Class ConcurrentActivityStateTest.
+ */
+public class ConcurrentActivityStateTest {
+    ProcessDefinition definition;
+    Activity startActivity;
+    Node startNode;
+
+    @Test
+    public void f() throws DalmatinaException {
+
+        // Create two process instances
+        NavigatorImplMock nav = new NavigatorImplMock();
+        ProcessInstance instance1 = new ProcessInstanceImpl(definition);
+        Token token1 = instance1.createToken(startNode, nav);
+
+        ProcessInstance instance2 = new ProcessInstanceImpl(definition);
+        Token token2 = instance2.createToken(startNode, nav);
+
+        assertEquals(token1.getCurrentNode().getActivity().getState(), ActivityState.INIT);
+        assertEquals(token2.getCurrentNode().getActivity().getState(), ActivityState.INIT);
+
+        // Execute a step with token1 on instance1. We expect, that the activity state changes for instance1, but not
+        // for instance2/token2.
+        
+        token1.executeStep();
+        assertEquals(startNode.getActivity().getState(), ActivityState.COMPLETED);
+        assertEquals(token2.getCurrentNode().getActivity().getState(), ActivityState.INIT);
+
+    }
+
+    @BeforeClass
+    public void setUpProcess()
+    throws IllegalStarteventException {
+
+        ProcessBuilder builder = new ProcessBuilderImpl();
+        NodeParameter param = new NodeParameterImpl();
+        startActivity = new NullActivity();
+        param.setActivity(startActivity);
+        param.setIncomingBehaviour(new SimpleJoinBehaviour());
+        param.setOutgoingBehaviour(new TakeAllSplitBehaviour());
+        param.makeStartNode(true);
+
+        startNode = builder.createNode(param);
+
+        param.setActivity(new NullActivity());
+        param.makeStartNode(false);
+        Node endNode = builder.createNode(param);
+
+        definition = builder.createTransition(startNode, endNode).buildDefinition();
+    }
+}
