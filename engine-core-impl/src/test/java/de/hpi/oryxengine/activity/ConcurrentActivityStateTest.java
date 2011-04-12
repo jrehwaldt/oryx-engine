b70a5ea1eb4d1daa944ece4344089dab674a6c51
thorben.lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2011-04-12 16:48:15 +0200

    Added more comments to explain the problem.

--- engine-core-impl/src/test/java/de/hpi/oryxengine/activity/ConcurrentActivityStateTest.java
+++ engine-core-impl/src/test/java/de/hpi/oryxengine/activity/ConcurrentActivityStateTest.java
@@ -22,7 +22,18 @@ import de.hpi.oryxengine.routing.behaviour.incoming.impl.SimpleJoinBehaviour;
 import de.hpi.oryxengine.routing.behaviour.outgoing.impl.TakeAllSplitBehaviour;
 
 /**
- * The Class ConcurrentActivityStateTest.
+ * The Class ConcurrentActivityStateTest. It tackles a problem with our process definition, nodes and the contained
+ * activities. If a node is executed several times, its activity is already "dirty" (e.g. its state is COMPLETED).
+ * 
+ * The Problem exists in the following cases: 1. Two instances work on the same process definition, execute the same
+ * activity 2. One token is in a loop and executes the activity twice
+ * 
+ * Possible solutions: 1. Create the node-Tree for each process instance (which implies activity recreation). This will
+ * only solve case 1 2. Create an activity instance, when the token reaches the node and only link some kind of activity
+ * blueprint to the node upon definition creation. This should solve both cases. Drawbacks are: Plugins cannot be
+ * registered as it is not clear when to do that (maybe keep them with the process instance, when did we do this before
+ * (not in a test)?). Deepcopys of activity members might be needed (e.g. the task that is assigned to the
+ * HumanTaskActivity has to be copied). I don't have a clue how to implement this cleanly.
  */
 public class ConcurrentActivityStateTest {
     private ProcessDefinition definition;
@@ -62,8 +73,9 @@ public class ConcurrentActivityStateTest {
 
     /**
      * Sets the up process.
-     *
-     * @throws IllegalStarteventException the illegal startevent exception
+     * 
+     * @throws IllegalStarteventException
+     *             the illegal startevent exception
      */
     @BeforeClass
     public void setUpProcess()
@@ -84,5 +96,6 @@ public class ConcurrentActivityStateTest {
         Node endNode = builder.createNode(param);
 
         definition = builder.createTransition(startNode, endNode).buildDefinition();
+
     }
 }
