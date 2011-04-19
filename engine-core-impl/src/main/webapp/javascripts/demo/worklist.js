d4d4e75aa065c2940954d764c92cb1fcb3ec45af
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-04-19 16:30:04 +0200

    Uncommented the Jannik file

--- engine-core-impl/src/main/webapp/javascripts/demo/worklist.js
+++ engine-core-impl/src/main/webapp/javascripts/demo/worklist.js
@@ -1,18 +1,18 @@
 $().ready(function(){
 
-//    // AJAX request to get the worklist
-//    $.ajax({
-//        type: 'GET',
-//        url: '/api/worklist/items',
-//        data: $.Storage.get("participantUUID"),
-//        success: function(data) {
-//            $('#worklist').html(data);
-//        },
-//        error: function(jqXHR, textStatus, errorThrown) {
-//            $('#worklist').html(jqXHR.responseText).addClass('error');
-//        },
-//        contentType: 'application/json', // we send json
-//        dataType: "json" // we expect json
+    // AJAX request to get the worklist
+    $.ajax({
+        type: 'GET',
+        url: '/api/worklist/items',
+        data: $.Storage.get("participantUUID"),
+        success: function(data) {
+            $('#worklist').html(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            $('#worklist').html(jqXHR.responseText).addClass('error');
+        },
+        contentType: 'application/json', // we send json
+        dataType: "json" // we expect json
     });
 })
 
