3f63363c3fd66522a171f3742807db9efbae99b3
JannikStreek <theshowcanbegin@gmail.com>
2011-05-09 11:26:12 +0200

    func refactoring of the benchmark.js since I fogot about the whole asynchronous thing

--- engine-web-gui-prototype/src/main/webapp/javascripts/benchmark/benchmark.js
+++ engine-web-gui-prototype/src/main/webapp/javascripts/benchmark/benchmark.js
@@ -10,53 +10,53 @@
 ******************************************************/
 
 // constants
-JODA_ENGINE_ADRESS = 'localhost';
+JODA_ENGINE_ADRESS = 'localhost:8380';
 
 // GET Land
 
 // gets all the available roles and returns them
-function getRoles() {
+function getRoles(func) {
 	$.ajax({
         type: 'GET',
         url: JODA_ENGINE_ADRESS + '/api/identity/roles',
         success: function(data) {
-            return data;
+            func(data);
         },
         dataType: "json"
     });
 }
 
 // get all the participants and return them
-function getParticipants() {
+function getParticipants(func) {
 	$.ajax({
         type: 'GET',
         url: JODA_ENGINE_ADRESS + '/api/identity/participants',
         success: function(data) {
-        	return data;
+        	func(data);
         },
         dataType: "json"
     });
 }
 
 // get all the deployed process definitions and return them
-function getProcessDefinitions() {
+function getProcessDefinitions(func) {
 	$.ajax({
         type: 'GET',
         url: JODA_ENGINE_ADRESS + '/api/repository/process-definitions',
         success: function(data) {
-        	return data;
+        	func(data);;
         },
         dataType: "json"
     });
 }
 
 // get all the running instances and return them and return them
-function getRunningProcessInstances() {
+function getRunningProcessInstances(func) {
 	$.ajax({
         type: 'GET',
         url: JODA_ENGINE_ADRESS + '/api/navigator/status/running-instances',
         success: function(data) {
-        	return data;
+        	func(data);
         },
         dataType: "json"
     });
