3638a858b4b8497fbf20346e5e057bf501ab0a3c
thorben.lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2011-05-09 11:48:05 +0200

    fixed some checkstyle warnings.

--- engine-web-gui-prototype/src/main/java/de/hpi/oryxengine/rest/demo/BenchmarkDeployer.java
+++ engine-web-gui-prototype/src/main/java/de/hpi/oryxengine/rest/demo/BenchmarkDeployer.java
@@ -30,46 +30,60 @@ import de.hpi.oryxengine.resource.allocation.pattern.SimplePullPattern;
 /**
  * This class deploys the benchmark process as specified in signavio.
  */
-public class BenchmarkDeployer {
+public final class BenchmarkDeployer {
     private static boolean invoked = false;
     private static final String PATH_TO_WEBFORMS = "src/main/resources/forms/benchmark";
-    
+
     private static IdentityService identityService = ServiceFactory.getIdentityService();
     private static ProcessDefinitionBuilder processDefinitionBuilder = new ProcessDefinitionBuilderImpl();
     private static IdentityBuilder identityBuilder = identityService.getIdentityBuilder();
 
     private static Role roleA, roleB, roleC, roleD, roleE;
-    
+
+    /**
+     * Hidden default constructor.
+     */
+    private BenchmarkDeployer() {
+
+    }
+
+    /**
+     * Creates five roles for the benchmark process.
+     */
     public static void createRoles() {
+
         roleA = (Role) identityBuilder.createRole("A");
         roleB = (Role) identityBuilder.createRole("B");
         roleC = (Role) identityBuilder.createRole("C");
         roleD = (Role) identityBuilder.createRole("D");
         roleE = (Role) identityBuilder.createRole("E");
     }
-    
-    public static void initializeNodes() throws DefinitionNotFoundException {
+
+    /**
+     * Creates the nodes as specified in the process model.
+     * 
+     * @throws DefinitionNotFoundException
+     *             the definition not found exception
+     */
+    public static void initializeNodes()
+    throws DefinitionNotFoundException {
+
         Node startNode = BpmnNodeFactory.createBpmnStartEventNode(processDefinitionBuilder);
-        
+
         Node andSplit1 = BpmnNodeFactory.createBpmnAndGatewayNode(processDefinitionBuilder);
         Node andSplit2 = BpmnNodeFactory.createBpmnAndGatewayNode(processDefinitionBuilder);
         Node andSplit3 = BpmnNodeFactory.createBpmnAndGatewayNode(processDefinitionBuilder);
         Node andJoin1 = BpmnNodeFactory.createBpmnAndGatewayNode(processDefinitionBuilder);
         Node andJoin2 = BpmnNodeFactory.createBpmnAndGatewayNode(processDefinitionBuilder);
         Node andJoin3 = BpmnNodeFactory.createBpmnAndGatewayNode(processDefinitionBuilder);
-        
+
         Form form = extractForm("dummyform", "dummy.html");
-        Task roleATask = createRoleTask("Do stuff", "Do it", form,
-            roleA);
-        Task roleBTask = createRoleTask("Do stuff", "Do it", form,
-            roleB);
-        Task roleCTask = createRoleTask("Do stuff", "Do it", form,
-            roleC);
-        Task roleDTask = createRoleTask("Do stuff", "Do it", form,
-            roleD);
-        Task roleETask = createRoleTask("Do stuff", "Do it", form,
-            roleE);
-        
+        Task roleATask = createRoleTask("Do stuff", "Do it", form, roleA);
+        Task roleBTask = createRoleTask("Do stuff", "Do it", form, roleB);
+        Task roleCTask = createRoleTask("Do stuff", "Do it", form, roleC);
+        Task roleDTask = createRoleTask("Do stuff", "Do it", form, roleD);
+        Task roleETask = createRoleTask("Do stuff", "Do it", form, roleE);
+
         Node activityA1 = BpmnNodeFactory.createBpmnUserTaskNode(processDefinitionBuilder, roleATask);
         Node activityB1 = BpmnNodeFactory.createBpmnUserTaskNode(processDefinitionBuilder, roleBTask);
         Node activityB2 = BpmnNodeFactory.createBpmnUserTaskNode(processDefinitionBuilder, roleBTask);
@@ -80,15 +94,15 @@ public class BenchmarkDeployer {
         Node activityD2 = BpmnNodeFactory.createBpmnUserTaskNode(processDefinitionBuilder, roleDTask);
         Node activityD3 = BpmnNodeFactory.createBpmnUserTaskNode(processDefinitionBuilder, roleDTask);
         Node activityE1 = BpmnNodeFactory.createBpmnUserTaskNode(processDefinitionBuilder, roleETask);
-        
+
         Node endNode = BpmnNodeFactory.createBpmnEndEventNode(processDefinitionBuilder);
-        
+
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, startNode, andSplit1);
-        
+
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, andSplit1, activityB1);
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, andSplit1, activityC1);
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, andSplit1, activityD1);
-        
+
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, activityB1, andSplit3);
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, andSplit3, activityB2);
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, andSplit3, activityA1);
@@ -96,10 +110,10 @@ public class BenchmarkDeployer {
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, activityB2, andJoin3);
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, andJoin3, activityB3);
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, activityB3, andJoin1);
-        
+
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, activityC1, activityC2);
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, activityC2, andJoin1);
-        
+
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, activityD1, andSplit2);
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, andSplit2, activityD2);
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, andSplit2, activityE1);
@@ -107,16 +121,26 @@ public class BenchmarkDeployer {
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, activityD2, andJoin2);
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, andJoin2, activityD3);
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, activityD3, andJoin1);
-        
+
         BpmnNodeFactory.createTransitionFromTo(processDefinitionBuilder, andJoin1, endNode);
-        
-        processDefinitionBuilder.setName("Benchmark Process").setDescription("This process has 5 roles, some parallel gateways and user tasks only.");
+
+        processDefinitionBuilder.setName("Benchmark Process").setDescription(
+            "This process has 5 roles, some parallel gateways and user tasks only.");
     }
-    
+
+    /**
+     * Creates roles, nodes and deploys the resulting process definition.
+     * 
+     * @throws IllegalStarteventException
+     *             the illegal startevent exception
+     * @throws DefinitionNotFoundException
+     *             the definition not found exception
+     * @throws ResourceNotAvailableException
+     *             the resource not available exception
+     */
     public static synchronized void generate()
     throws IllegalStarteventException, DefinitionNotFoundException, ResourceNotAvailableException {
 
-       
         if (!invoked) {
             createRoles();
             initializeNodes();
@@ -126,7 +150,18 @@ public class BenchmarkDeployer {
             invoked = true;
         }
     }
-    
+
+    /**
+     * Extracts a form.
+     * 
+     * @param formName
+     *            the form name
+     * @param formPath
+     *            the form path
+     * @return the form
+     * @throws DefinitionNotFoundException
+     *             the definition not found exception
+     */
     private static Form extractForm(String formName, String formPath)
     throws DefinitionNotFoundException {
 
@@ -137,7 +172,7 @@ public class BenchmarkDeployer {
         form = new FormImpl(ServiceFactory.getRepositoryService().getProcessResource(processArtifactID));
         return form;
     }
-    
+
     private static Task createRoleTask(String subject, String description, Form form, AbstractResource<?> resource) {
 
         AllocationStrategies allocationStrategies = new AllocationStrategiesImpl(new RolePushPattern(),
@@ -145,7 +180,7 @@ public class BenchmarkDeployer {
 
         return createTask(subject, description, form, allocationStrategies, resource);
     }
-    
+
     private static Task createTask(String subject,
                                    String description,
                                    Form form,
