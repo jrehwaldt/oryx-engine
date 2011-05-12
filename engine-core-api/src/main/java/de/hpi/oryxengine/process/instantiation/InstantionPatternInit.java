68494fc30b68d6764475ab06f219980e343bbb72
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2011-05-12 16:02:09 +0200

    Merged

---
+++ engine-core-api/src/main/java/de/hpi/oryxengine/process/instantiation/InstantionPatternInit.java
@@ -0,0 +1,31 @@
+package de.hpi.oryxengine.process.instantiation;
+
+import de.hpi.oryxengine.correlation.CorrelationManager;
+import de.hpi.oryxengine.navigator.NavigatorInside;
+import de.hpi.oryxengine.process.definition.ProcessDefinitionInside;
+
+/**
+ * This class is only the shared foundation of classes {@link StartInstantiationPattern} and
+ * {@link ProcessInstantiationPattern}.
+ * 
+ * Both patterns need to have an initialization method in order to setup Variables.
+ * 
+ */
+public interface InstantionPatternInit {
+
+    /**
+     * Setups the variables for the {@link InstantionPatternInit}.
+     * 
+     * @param correlationManager
+     *            - a {@link CorrelationManager}
+     * @param navigator
+     *            - a {@link NavigatorInside navigator}
+     * @param processDefinition
+     *            - the {@link ProcessDefinitionInside} the method belongs to
+     * @return the current {@link InstantionPatternInit} which in most cases will be a {@link StartInstantiationPattern}
+     *         or a {@link ProcessInstantiationPattern}
+     */
+    InstantionPatternInit init(CorrelationManager correlationManager,
+                               NavigatorInside navigator,
+                               ProcessDefinitionInside processDefinition);
+}
