3f15c97d8bf6770a6df352368f4b5058fb88942f
thorben.lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2011-05-13 11:57:19 +0200

    wrote comments, removed TODO.

--- engine-core-impl/src/main/java/de/hpi/oryxengine/node/activity/bpmn/BpmnHumanTaskActivity.java
+++ engine-core-impl/src/main/java/de/hpi/oryxengine/node/activity/bpmn/BpmnHumanTaskActivity.java
@@ -1,6 +1,5 @@
 package de.hpi.oryxengine.node.activity.bpmn;
 
-import java.util.Iterator;
 import java.util.List;
 
 import javax.annotation.Nonnull;
@@ -13,48 +12,40 @@ import de.hpi.oryxengine.allocation.PushPattern;
 import de.hpi.oryxengine.allocation.TaskAllocation;
 import de.hpi.oryxengine.node.activity.AbstractActivity;
 import de.hpi.oryxengine.process.token.Token;
-import de.hpi.oryxengine.resource.AbstractResource;
-import de.hpi.oryxengine.resource.allocation.pattern.AllocateSinglePattern;
-import de.hpi.oryxengine.resource.worklist.AbstractDefaultWorklist;
 import de.hpi.oryxengine.resource.worklist.AbstractWorklistItem;
-import de.hpi.oryxengine.resource.worklist.WorklistItemImpl;
 
 /**
  * The Implementation of a human task.
  * 
  * A user task is a typical workflow Task where a human performer performs a task with the assistance of a software
- * application.
+ * application. Upon its execution, worklist items are created with a {@link CreationPattern} and the distributed with a
+ * {@link PushPattern}.
  */
 public class BpmnHumanTaskActivity extends AbstractActivity {
 
     @JsonProperty
     private CreationPattern creationPattern;
-    
+
     @JsonProperty
     private PushPattern pushPattern;
 
     /**
      * Default Constructor.
      * 
-     * @param task
-     *            - the task to distribute
+     * @param creationPattern
+     *            the creation pattern to use
+     * @param pushPattern
+     *            the push pattern to use
      */
-    // TODO: CreationPattern einfügen
-    public BpmnHumanTaskActivity(CreationPattern pattern, PushPattern pushPattern) {
+    public BpmnHumanTaskActivity(CreationPattern creationPattern, PushPattern pushPattern) {
 
-        this.creationPattern = pattern;
+        this.creationPattern = creationPattern;
         this.pushPattern = pushPattern;
     }
 
     @Override
     protected void executeIntern(@Nonnull Token token) {
 
-        // creationPattern.createTask()
-
-        // TODO @Thorben-Refactoring think about TaskDistribution and its necessity
-//        TaskDistribution taskDistribution = ServiceFactory.getTaskDistribution();
-//        taskDistribution.distribute(task, token);
-        // TODO @Thorben-Refactoring should we use the TaskAllocation here?
         TaskAllocation service = ServiceFactory.getWorklistQueue();
         List<AbstractWorklistItem> items = creationPattern.createWorklistItems(token);
         pushPattern.distributeWorkitems(service, items);
@@ -65,22 +56,23 @@ public class BpmnHumanTaskActivity extends AbstractActivity {
     @Override
     public void cancel() {
 
-        // TODO change this as soon as we do not have the separation of task/worklistitem anymore (use methods from TaskAllocation)
+        // TODO change this as soon as we do not have the separation of task/worklistitem anymore (use methods from
+        // TaskAllocation)
         // TODO add this again, but maybe extend the creationPattern, to be able to remove the worklist items as well
-//        for (AbstractResource<?> resource : creationPattern.getAssignedResources()) {
-//            // remove all offered items
-//            Iterator<AbstractWorklistItem> it = ((AbstractDefaultWorklist) resource.getWorklist())
-//            .getLazyWorklistItems().iterator();
-//
-//            while (it.hasNext()) {
-//                WorklistItemImpl item = (WorklistItemImpl) it.next();
-//                if (item == task) {
-//                    it.remove();
-//                }
-//            }
-//        }
-        
-//        ServiceFactory.getWorklistQueue().removeWorklistItem(task, task.getAssignedResources());
+        // for (AbstractResource<?> resource : creationPattern.getAssignedResources()) {
+        // // remove all offered items
+        // Iterator<AbstractWorklistItem> it = ((AbstractDefaultWorklist) resource.getWorklist())
+        // .getLazyWorklistItems().iterator();
+        //
+        // while (it.hasNext()) {
+        // WorklistItemImpl item = (WorklistItemImpl) it.next();
+        // if (item == task) {
+        // it.remove();
+        // }
+        // }
+        // }
+
+        // ServiceFactory.getWorklistQueue().removeWorklistItem(task, task.getAssignedResources());
 
     }
 }
