efd9b18c63be674056b23e5ea01c59582d7c459f
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2011-05-16 16:47:30 +0200

    JavaDoc added

--- engine-core-impl/src/main/java/org/jodaengine/plugin/navigator/NavigatorListenerLogger.java
+++ engine-core-impl/src/main/java/org/jodaengine/plugin/navigator/NavigatorListenerLogger.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 /**
  * A logger implementation, which observes the state of a navigator thread.
  */
-public class NavigatorListenerLogger
+public final class NavigatorListenerLogger
 extends AbstractNavigatorListener {
     
     private static NavigatorListenerLogger instance;
