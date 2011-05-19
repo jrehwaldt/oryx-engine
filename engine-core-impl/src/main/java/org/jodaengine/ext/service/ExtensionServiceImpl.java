37a234d001d09c95146b5d58acd23d5c7bd145b4
jan.rehwaldt <jan.rehwaldt@student.hpi.uni-potsdam.de>
2011-05-19 22:03:13 +0200

    * cleanup and extract methods in ExtensionService

--- engine-core-impl/src/main/java/org/jodaengine/ext/service/ExtensionServiceImpl.java
+++ engine-core-impl/src/main/java/org/jodaengine/ext/service/ExtensionServiceImpl.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Nonnull;
+
 import org.jodaengine.JodaEngineServices;
 import org.jodaengine.bootstrap.Service;
 import org.jodaengine.ext.Extension;
@@ -102,7 +104,6 @@ public class ExtensionServiceImpl implements ExtensionService {
         return this.extensions.contains(extension);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public <IExtension> List<IExtension> getExtensions(Class<IExtension> extension) {
         
@@ -111,70 +112,16 @@ public class ExtensionServiceImpl implements ExtensionService {
         //
         // create an instance of each requested extension
         //
-        createInstance : for (Class<IExtension> ext: getExtensionClasses(extension)) {
+        for (Class<IExtension> ext: getExtensionClasses(extension)) {
             
             //
-            // get a public constructor
+            // create an instance with the injected parameters
             //
-            Constructor<IExtension>[] constructors = (Constructor<IExtension>[]) ext.getConstructors();
-            for (Constructor<IExtension> constructor: constructors) {
-                Type[] parameterTypes = constructor.getGenericParameterTypes();
-                Object[] parameters = new Object[parameterTypes.length];
-                
-                //
-                // set parameter for certain services, if defined in constructor
-                //
-                setParameters : for (int i = 0; i < parameterTypes.length; i++) {
-                    Type paramaterType = parameterTypes[i];
-                    
-                    if (paramaterType instanceof Class<?>) {
-                        Class<?> parameterClass = (Class<?>) paramaterType;
-                        
-                        //
-                        // inject the JodaEngineServices
-                        //
-                        if (JodaEngineServices.class.isAssignableFrom(parameterClass)) {
-                            parameters[i] = this.coreServices;
-                            continue;
-                        }
-                        
-                        //
-                        // inject the ExtensionService
-                        //
-                        if (ExtensionService.class.isAssignableFrom(parameterClass)) {
-                            parameters[i] = this;
-                            continue;
-                        }
-                        
-                        //
-                        // inject other available services
-                        //
-                        if (Service.class.isAssignableFrom(parameterClass)) {
-                            
-                            for (Service service: this.extensionServices.values()) {
-                                if (parameterClass.isAssignableFrom(service.getClass())) {
-                                    parameters[i] = service;
-                                    continue setParameters;
-                                }
-                            }
-                        }
-                        
-                        //
-                        // unknown parameters are provided as null
-                        //
-                        parameters[i] = null;
-                    }
-                }
-                
-                //
-                // create an instance with the injected parameters
-                //
-                try {
-                    instances.add(constructor.newInstance(parameters));
-                    continue createInstance;
-                } catch (Exception e) {
-                    logger.error("Extension could not be instantiated.", e);
-                }
+            try {
+                instances.add(createExtensionInstance(ext));
+                continue;
+            } catch (ExtensionNotAvailableException e) {
+                logger.error("No such extension available. None of constructors could be instantiated.");
             }
         }
         
@@ -214,24 +161,11 @@ public class ExtensionServiceImpl implements ExtensionService {
                 continue;
             }
             
+            //
+            // create the service and keep a reference
+            //
             try {
-                //
-                // start the service and keep a reference
-                //
-                try {
-                    Constructor<Service> serviceConstructor = serviceClass.getConstructor(JodaEngineServices.class);
-                    Service serviceInstance = serviceConstructor.newInstance(this.coreServices);
-                    this.extensionServices.put(name, serviceInstance);
-                    continue;
-                } catch (NoSuchMethodException nsme) {
-                    //
-                    // No public constructor with JodaEngineServices as parameter found.
-                    // Assume empty public constructor.
-                    //
-                    Service serviceInstance = serviceClass.newInstance();
-                    this.extensionServices.put(name, serviceInstance);
-                }
-                
+                this.extensionServices.put(name, createExtensionInstance(serviceClass));
             } catch (Exception e) {
                 logger.error("Could not instantiate extension service.", e);
             }
@@ -253,12 +187,12 @@ public class ExtensionServiceImpl implements ExtensionService {
      * @return the classes matching the extension point's interface
      */
     @SuppressWarnings("unchecked")
-    private <IExtension> List<Class<IExtension>> getExtensionClasses(Class<IExtension> extension) {
+    private @Nonnull <IExtension> List<Class<IExtension>> getExtensionClasses(@Nonnull Class<IExtension> extension) {
         
         List<Class<IExtension>> classes = new ArrayList<Class<IExtension>>();
         
         //
-        // find class candidates
+        // class path scanner
         //
         ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
         
@@ -271,14 +205,99 @@ public class ExtensionServiceImpl implements ExtensionService {
         
         scanner.addIncludeFilter(filter);
         
+        //
+        // search for the specified beans within base package scope
+        //
         for (BeanDefinition bd : scanner.findCandidateComponents(BASE_PACKAGE)) {
             try {
                 classes.add((Class<IExtension>) Class.forName(bd.getBeanClassName()));
             } catch (ClassNotFoundException cnfe) {
+                //
+                // this is seriously unlikely to happen... we found it in the class path just a second ago
+                //
                 logger.error("Extension class could not be found", cnfe);
             }
         }
         
         return classes;
+    }
+    
+    /**
+     * This method constructs an instance of the required {@link Extension}'s interface
+     * providing it with any kind of {@link Service} specified in the constructor.
+     * 
+     * @param <IExtension> the extension's interface
+     * @param extension the extension's interface
+     * @return an instance of the extension
+     * @throws ExtensionNotAvailableException in case the extension could not be instantiated
+     */
+    @SuppressWarnings("unchecked")
+    private @Nonnull <IExtension> IExtension createExtensionInstance(@Nonnull Class<IExtension> extension)
+    throws ExtensionNotAvailableException {
+        
+        //
+        // get a public constructor
+        //
+        Constructor<IExtension>[] constructors = (Constructor<IExtension>[]) extension.getConstructors();
+        for (Constructor<IExtension> constructor: constructors) {
+            Type[] parameterTypes = constructor.getGenericParameterTypes();
+            Object[] parameters = new Object[parameterTypes.length];
+            
+            //
+            // set parameter for certain services, if defined in constructor
+            //
+            setParameters : for (int i = 0; i < parameterTypes.length; i++) {
+                Type paramaterType = parameterTypes[i];
+                
+                if (paramaterType instanceof Class<?>) {
+                    Class<?> parameterClass = (Class<?>) paramaterType;
+                    
+                    //
+                    // inject the JodaEngineServices
+                    //
+                    if (JodaEngineServices.class.isAssignableFrom(parameterClass)) {
+                        parameters[i] = this.coreServices;
+                        continue;
+                    }
+                    
+                    //
+                    // inject the ExtensionService
+                    //
+                    if (ExtensionService.class.isAssignableFrom(parameterClass)) {
+                        parameters[i] = this;
+                        continue;
+                    }
+                    
+                    //
+                    // inject other available services
+                    //
+                    if (Service.class.isAssignableFrom(parameterClass)) {
+                        
+                        for (Service service: this.extensionServices.values()) {
+                            if (parameterClass.isAssignableFrom(service.getClass())) {
+                                parameters[i] = service;
+                                continue setParameters;
+                            }
+                        }
+                    }
+                    
+                    //
+                    // unknown parameters are provided as null
+                    //
+                    parameters[i] = null;
+                }
+            }
+            
+            //
+            // create an instance with the injected parameters
+            //
+            try {
+                return constructor.newInstance(parameters);
+            } catch (Exception e) {
+                logger.warn("Extension could not be instantiated.", e);
+            }
+        }
+        
+        throw new ExtensionNotAvailableException();
     }
 }
