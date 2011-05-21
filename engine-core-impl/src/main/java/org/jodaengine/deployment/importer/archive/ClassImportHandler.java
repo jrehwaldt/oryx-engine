ab9dcbcb8fbdea0b4b89b728ea602d4a36f3e046
thorben.lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2011-05-21 13:07:16 +0200

    Added necessary test-resources and did a minor change.

--- engine-core-impl/src/main/java/org/jodaengine/deployment/importer/archive/ClassImportHandler.java
+++ engine-core-impl/src/main/java/org/jodaengine/deployment/importer/archive/ClassImportHandler.java
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory;
  * Imports classes from a .dar-file, that are located in the /classes directory and any sub-directories.
  */
 public class ClassImportHandler extends AbstractDarHandler {
-    
+
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private static final String CLASSES_SUBDIR = "classes/";
     private static final String CLASS_ENDING = ".class";
@@ -21,25 +21,22 @@ public class ClassImportHandler extends AbstractDarHandler {
     @Override
     public void processSingleDarFileEntry(ZipFile darFile, ZipEntry entry, DeploymentBuilder builder) {
 
-        // TODO remove this when implementation is finished
-        System.out.println(entry.getName());
-
         if (entry.getName().startsWith(CLASSES_SUBDIR) && !entry.isDirectory()) {
             try {
                 byte[] classData = new byte[(int) entry.getSize()];
                 BufferedInputStream inputStream = new BufferedInputStream(darFile.getInputStream(entry));
                 inputStream.read(classData);
-                
-                String fullClassName = entry.getName().substring(CLASSES_SUBDIR.length(), entry.getName().length() - CLASS_ENDING.length());
+
+                int prefixLength = CLASSES_SUBDIR.length();
+                int suffixLength = entry.getName().length() - CLASS_ENDING.length();
+                String fullClassName = entry.getName().substring(prefixLength, suffixLength);
                 fullClassName = fullClassName.replace('/', '.');
                 builder.addClass(fullClassName, classData);
-                
-                //inputStream.close();
             } catch (IOException e) {
                 logger.error("Could not read file {} from archive", entry.getName());
             }
         }
-        
+
     }
 
 }
