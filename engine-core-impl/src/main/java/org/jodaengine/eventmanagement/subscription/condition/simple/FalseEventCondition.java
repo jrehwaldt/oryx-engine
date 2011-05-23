26a4861d5c7b8910ec39177cae97b480f97a21c5
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2011-05-23 15:37:37 +0200

    Merged mit Master

--- engine-core-impl/src/main/java/org/jodaengine/eventmanagement/subscription/condition/simple/FalseEventCondition.java
+++ engine-core-impl/src/main/java/org/jodaengine/eventmanagement/subscription/condition/simple/FalseEventCondition.java
@@ -4,7 +4,8 @@ import org.jodaengine.eventmanagement.AdapterEvent;
 import org.jodaengine.eventmanagement.subscription.condition.EventCondition;
 
 /**
- * TODO @EVENTMANAGERTEAM and why again do we need it?
+ * TODO @EVENTMANAGERTEAM and why again do we need it? - It is a basic condition and we need it for testing.
+ * 
  * A Condition that never holds. It always returns false. 
  */
 public class FalseEventCondition implements EventCondition {
