8e0a2e4f1f42563e6a9d8540a6f9493373006ec9
jan.rehwaldt <jan.rehwaldt@student.hpi.uni-potsdam.de>
2011-05-25 21:47:49 +0200

    * fix gitignore * git ist völlig beknackt - Amokgit!

---
+++ engine-ext-debugger/src/main/java/org/jodaengine/ext/debugging/api/BreakpointCondition.java
@@ -0,0 +1,25 @@
+package org.jodaengine.ext.debugging.api;
+
+import javax.annotation.Nonnull;
+
+import org.jodaengine.process.token.Token;
+
+/**
+ * This interface represents a condition, which may be attached to a {@link Breakpoint}.
+ * 
+ * @author Jan Rehwaldt
+ * @since 2011-05-24
+ */
+public interface BreakpointCondition {
+    
+    /**
+     * When this method is called the condition needs to be evaluated.
+     * If it evaluates to true this is returned, false respectively.
+     * 
+     * True is furthermore returned if the condition fails to be evaluated.
+     * 
+     * @param token the token
+     * @return whether the condition is true
+     */
+    boolean evaluate(@Nonnull Token token);
+}
