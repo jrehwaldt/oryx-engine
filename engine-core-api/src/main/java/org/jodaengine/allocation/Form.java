cca54da4569656e743370a032dc85ce404485673
Gerardo Navarro Suarez <gerardo.navarro-suarez@student.hpi.uni-potsdam.de>
2011-05-26 16:18:25 +0200

    Leerzeichen eingefügt, um Punkt auf dem Hudson zu kriegen

--- engine-core-api/src/main/java/org/jodaengine/allocation/Form.java
+++ engine-core-api/src/main/java/org/jodaengine/allocation/Form.java
@@ -17,7 +17,7 @@ public interface Form {
      * 
      * @return a {@link String} representing the form content in the HTML format
      */
-    String getFormContentAsHTML();
+    String getFormContentAsHTML(); 
     
     /**
      * Gets a form field with the specified name.
