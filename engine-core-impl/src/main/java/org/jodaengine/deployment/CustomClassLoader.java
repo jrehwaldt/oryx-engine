d05a860914eea909d80a332585459a4f14332e7a
thorben.lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2011-06-06 10:43:04 +0200

    wrote test for script loading.

--- engine-core-impl/src/main/java/org/jodaengine/deployment/CustomClassLoader.java
+++ engine-core-impl/src/main/java/org/jodaengine/deployment/CustomClassLoader.java
@@ -3,12 +3,16 @@ package org.jodaengine.deployment;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * A ClassLoader for the JodaEngine, that serves classes, that were defined in a deployment package.
  */
 public class CustomClassLoader extends ClassLoader implements ClassContainer {
 
     private Map<String, byte[]> classes;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     
     /**
      * Instantiates a new custom class loader.
@@ -20,14 +24,22 @@ public class CustomClassLoader extends ClassLoader implements ClassContainer {
     
     @Override
     public Class<?> findClass(String name) throws ClassNotFoundException {
+        logger.debug("This method has been called");
         Class<?> classToReturn = findLoadedClass(name);
+        logger.debug("Searching for class {}", name);
         if (classToReturn == null) {
-            byte[] classAsBytes = classes.get(name);     
-            
-            if (classAsBytes == null) {
-                throw new ClassNotFoundException();
+            try {
+                classToReturn = findSystemClass(name);
+            } catch (ClassNotFoundException e) {
+                byte[] classAsBytes = classes.get(name);     
+                
+                if (classAsBytes == null) {
+                    logger.error("Class {} could not be found", name);
+                    throw new ClassNotFoundException();
+                }
+                classToReturn = defineClass(name, classAsBytes, 0, classAsBytes.length);
             }
-            classToReturn = defineClass(name, classAsBytes, 0, classAsBytes.length);
+            
         }
         return classToReturn;
     }
