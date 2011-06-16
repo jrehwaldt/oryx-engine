19fd3d5bbedce4f677db18e945202a76083eb9c0
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-06-16 12:01:58 +0200

    Created IntermediateIncomingImapEmailProcessEvent

---
+++ engine-core-impl/src/main/java/org/jodaengine/eventmanagement/processevent/incoming/intermediate/IntermediateIncomingImapEmailProcessEvent.java
@@ -0,0 +1,32 @@
+package org.jodaengine.eventmanagement.processevent.incoming.intermediate;
+
+import org.jodaengine.eventmanagement.adapter.configuration.AdapterConfiguration;
+import org.jodaengine.eventmanagement.subscription.condition.EventCondition;
+import org.jodaengine.process.token.Token;
+
+/**
+ * The intermeidate Incoming Imap-Email process event which wiats for a specific email to arrive.
+ */
+public class IntermediateIncomingImapEmailProcessEvent extends AbstractIncomingIntermediateProcessEvent {
+
+    /**
+     * Instantiates a new intermediate incoming imap email process event.
+     * 
+     * This event is used to wait for a specific email specified by the EventCondition and the AdapterConfiguration.
+     * 
+     * @param config
+     *            the configuration of the mailaccount (username, password etc..)
+     *            {@link IncomingMailAdapterConfiguration}
+     * @param condition
+     *            the condition specifying the properties an incoming has to statisfy in order for this event to fire.
+     * @param token
+     *            the Token for process execution
+     */
+    public IntermediateIncomingImapEmailProcessEvent(AdapterConfiguration config, 
+                                                     EventCondition condition, 
+                                                     Token token) {
+
+        super(config, condition, token);
+    }
+
+}
