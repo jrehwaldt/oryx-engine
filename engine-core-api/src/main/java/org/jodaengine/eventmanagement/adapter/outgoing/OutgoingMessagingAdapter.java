2e42d95a9d7e780ca4a5b1e673881d8202c3346f
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-06-17 18:28:42 +0200

    Forgot to add something

---
+++ engine-core-api/src/main/java/org/jodaengine/eventmanagement/adapter/outgoing/OutgoingMessagingAdapter.java
@@ -0,0 +1,20 @@
+package org.jodaengine.eventmanagement.adapter.outgoing;
+
+import org.jodaengine.eventmanagement.adapter.EventAdapter;
+import org.jodaengine.eventmanagement.adapter.Message;
+
+
+/**
+ * This interface should be implemented by {@link EventManagerService} adapters,
+ * which allow outgoing communication. This includes,
+ * e.g. email sending adapter.
+ */
+public interface OutgoingMessagingAdapter extends EventAdapter {
+
+   /**
+    * Send the message.
+    *
+    * @param message the message
+    */
+   void sendMessage(Message message);
+}
