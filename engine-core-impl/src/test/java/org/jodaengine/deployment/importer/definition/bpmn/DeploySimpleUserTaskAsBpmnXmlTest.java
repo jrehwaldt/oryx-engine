9f17089795db5866681fb89f36c54a739b2b4092
thorben.lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2011-06-20 15:19:01 +0200

    Added the parsing of roles to the BpmnXmlParse.

--- engine-core-impl/src/test/java/org/jodaengine/deployment/importer/definition/bpmn/DeploySimpleUserTaskAsBpmnXmlTest.java
+++ engine-core-impl/src/test/java/org/jodaengine/deployment/importer/definition/bpmn/DeploySimpleUserTaskAsBpmnXmlTest.java
@@ -10,6 +10,9 @@ import org.jodaengine.node.activity.bpmn.BpmnStartEvent;
 import org.jodaengine.process.definition.ProcessDefinition;
 import org.jodaengine.process.structure.Node;
 import org.jodaengine.resource.AbstractParticipant;
+import org.jodaengine.resource.AbstractRole;
+import org.jodaengine.resource.IdentityBuilder;
+import org.jodaengine.resource.IdentityService;
 import org.jodaengine.resource.allocation.CreationPattern;
 import org.jodaengine.resource.allocation.pattern.creation.AbstractCreationPattern;
 import org.testng.Assert;
@@ -17,7 +20,7 @@ import org.testng.annotations.BeforeMethod;
 
 /**
  * It tests the deployment of BPMN processes that where serialized as xml. The xml contains the structure the process
- * and additional information like DI-Information. The process contains only one UserTask that is associated to Jannik
+ * and additional information like DI-Information. The process contains only one UserTask that is associated to Thorben
  * telling him to get Gerardo a cup of coffee.
  * 
  * The process were modeled and exported as xml using academic signavio. The process contains a simple sequence.
@@ -30,6 +33,7 @@ import org.testng.annotations.BeforeMethod;
 public class DeploySimpleUserTaskAsBpmnXmlTest extends AbstractBPMNDeployerTest {
 
     private AbstractParticipant thorben;
+    private AbstractRole aRole;
 
     /**
      * Instantiates a new deploy simple user task as bpmn xml test, setting the path to the XML representation.
@@ -45,7 +49,9 @@ public class DeploySimpleUserTaskAsBpmnXmlTest extends AbstractBPMNDeployerTest 
     @BeforeMethod
     public void setUp() {
 
-        thorben = ServiceFactory.getIdentityService().getIdentityBuilder().createParticipant("Thorben");
+        IdentityBuilder identityBuilder = ServiceFactory.getIdentityService().getIdentityBuilder();
+        thorben = identityBuilder.createParticipant("Thorben");
+        aRole = identityBuilder.createRole("A Role");
     }
 
     @Override
@@ -59,6 +65,7 @@ public class DeploySimpleUserTaskAsBpmnXmlTest extends AbstractBPMNDeployerTest 
         Assert.assertEquals(onlyStartNode.getAttribute("name"), "Start");
         Assert.assertEquals(onlyStartNode.getOutgoingControlFlows().size(), 1);
 
+        // Check the first user task
         Node nextNode = onlyStartNode.getOutgoingControlFlows().get(0).getDestination();
         Assert.assertEquals(extractActivityClass(nextNode), BpmnHumanTaskActivity.class);
         Assert.assertEquals(nextNode.getAttribute("name"), "Thorben, please process this task!");
@@ -73,6 +80,23 @@ public class DeploySimpleUserTaskAsBpmnXmlTest extends AbstractBPMNDeployerTest 
         Assert.assertEquals(pattern.getAssignedResources().iterator().next(), thorben);
 
         Assert.assertEquals(nextNode.getOutgoingControlFlows().size(), 1);
+        
+        // check the second user task
+        nextNode = nextNode.getOutgoingControlFlows().get(0).getDestination();
+        Assert.assertEquals(extractActivityClass(nextNode), BpmnHumanTaskActivity.class);
+        Assert.assertEquals(nextNode.getAttribute("name"), "A Role, please process this task!");
+        Assert.assertEquals(nextNode.getAttribute("description"), "It is only a demo task.");
+
+        // Asserting the task
+        pattern = (AbstractCreationPattern) extractCreationPattern(nextNode);
+
+        Assert.assertEquals(pattern.getItemSubject(), "A Role, please process this task!");
+        Assert.assertEquals(pattern.getItemDescription(), "It is only a demo task.");
+        Assert.assertEquals(pattern.getItemFormID(), "form2.html");
+        Assert.assertEquals(pattern.getAssignedResources().iterator().next(), aRole);
+
+        Assert.assertEquals(nextNode.getOutgoingControlFlows().size(), 1);
+        
 
         Node endNode = nextNode.getOutgoingControlFlows().get(0).getDestination();
         Assert.assertEquals(extractActivityClass(endNode), BpmnEndEventActivity.class);
